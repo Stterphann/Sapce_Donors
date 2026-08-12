3e88ea04d5e1e806e7bac2f5b57300b17452193c
Lucas_DEV <101019769+Stterphann@users.noreply.github.com>
2026-08-12 14:49:16 -0300

    Delete script3.py

--- script3.py
+++
@@ -1,5 +0,0 @@
-# Let's inspect which image is which
-# input_file_0.png, input_file_1.png, Nave.gif, image_134a3e.png, Space Junkies...
-for name in ['input_file_0.png', 'input_file_1.png', 'Nave.gif', 'image_134a3e.png']:
-    if os.path.exists(name):
-        print(name, os.path.getsize(name))
